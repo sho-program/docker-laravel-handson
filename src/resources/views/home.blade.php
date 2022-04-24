8f62598f534c55b3ebd9b4fb18fd67f9483ebf69
Aso2001195 <2001195@s.asojuku.ac.jp>
2022-04-24 13:07:16 +0900

    web.php

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>トップ画面</title>
+    </head>
+    <body>
+        <p>こんにちは さようなら</p>
+    </body>
+</html>
