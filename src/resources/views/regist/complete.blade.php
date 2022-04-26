461dd5562b5c506f17cc021ad601d37668e602f9
Aso2001195 <2001195@s.asojuku.ac.jp>
2022-04-26 17:44:52 +0900

    first commit

---
+++ src/resources/views/regist/complete.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <title>登録完了画面</title>
+    </head>
+    <body>
+        {{ $user->name }}さんを登録しました。
+    </body>
+</html>
